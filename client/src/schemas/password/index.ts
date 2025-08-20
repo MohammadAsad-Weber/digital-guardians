42e750b97901d769043fed6aa5fd8d957a9a3338
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-08-20 15:24:07 +0530

    Password validation schemas

---
+++ client/src/schemas/password/index.ts
@@ -0,0 +1,15 @@
+import {
+  CreatePasswordSchema,
+  type CreatePassword,
+} from "./create-password.schema";
+import {
+  UpdatePasswordSchema,
+  type UpdatePassword,
+} from "./update-password.schema";
+
+export {
+  CreatePasswordSchema,
+  CreatePassword,
+  UpdatePasswordSchema,
+  UpdatePassword,
+};
