d128c8013bd4ce63ac64740c983de8e9716d3768
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-08-20 15:23:43 +0530

    User validation schemas

---
+++ client/src/schemas/user/index.ts
@@ -0,0 +1,15 @@
+import { UpdateUserSchema, type UpdateUser } from "./update-user.schema";
+import { DeleteUserSchema, type DeleteUser } from "./delete-user.schema";
+import {
+  ChangePasswordSchema,
+  type ChangePassword,
+} from "./change-password.schema";
+
+export {
+  UpdateUserSchema,
+  UpdateUser,
+  ChangePasswordSchema,
+  ChangePassword,
+  DeleteUserSchema,
+  DeleteUser,
+};
