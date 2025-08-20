bd694fa52dddd7774efcb783eb79d52e273eed43
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-08-20 15:42:42 +0530

    [Refactored]: Modified code for better functionality

--- client/src/pages/vault/password.tsx
+++ client/src/pages/vault/password.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import usePassword from "@/hooks/usePassword";
 import { Link, useParams } from "react-router";
-import { useQuery } from "@tanstack/react-query";
-import BackendError from "@/utilities/BackendError";
 
-// Utility functions
-import getFavicon from "@/utilities/getFavicon";
-import handleCopy from "@/utilities/handleCopy";
+// Services & Types for Password Data
+import { useQuery } from "@tanstack/react-query";
+import { getPassword } from "@/services/password";
+import type { PasswordData } from "@/types/response";
+
+// Utilities & Helper Functions
+import { mergeClassNames } from "@/libs";
+import { BackendError, getFavicon, handleCopy } from "@/utilities";
 
 // React-icons
 import { FiCopy } from "react-icons/fi";
@@ -14,40 +16,40 @@ import { IoClose } from "react-icons/io5";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 
 // Components
-import Spinner from "@/components/Spinner";
-import ErrorPage from "@/components/ErrorPage";
+import Spinner from "@/components/spinner";
+import ErrorPage from "@/components/error-page";
 
 function Password() {
-  // Hooks
-  const { id } = useParams();
-  const { getPassword } = usePassword();
+  // Extract route parameter
+  const { id } = useParams<{ id: string }>();
+
+  // State for password visibility toggle
   const [toggle, setToggle] = useState(false);
+
+  // Fetch password data
   const { status, data, error } = useQuery({
     queryKey: ["passwords", { id }],
-    queryFn: () => getPassword(id ?? ""),
+    queryFn: () => getPassword(id as string),
   });
 
+  // Pending state
   if (status === "pending") return <Spinner />;
-  if (status === "error") {
-    // constant variables
-    const backendError = error as BackendError;
-    const statusCode = backendError?.status_code ?? 500;
-    const status = backendError?.status ?? "Internal server error";
-    const message = backendError?.message ?? "An unknown error has occured";
 
+  // Error stata
+  if (status === "error") {
+    const backendError = error as BackendError;
     return (
       <ErrorPage
-        code={statusCode}
-        status={status}
-        message={message}
+        status={backendError.status}
+        status_code={backendError.status_code}
+        message={backendError.message}
       />
-    )
+    );
   }
-  if (data) {
-    // constant variables
-    const website = new URL(data.siteURL);
-    const favicon = getFavicon(website.hostname);
-
+  // Success state
+  if (status === "success") {
+    const passwordData = data as PasswordData;
+    const website = new URL(passwordData.siteURL);
     return (
       <main className="min-h-screen w-full p-5 flex items-center justify-center">
         <div className="max-w-md w-full p-5 pt-7.5 relative flex flex-col items-center justify-center gap-5 bg-white rounded-2xl shadow-[0_0_10px_#00000040] animate-zoom-out">
@@ -62,43 +64,48 @@ function Password() {
 
           {/* TITLE */}
           <div className="w-full flex items-center justify-start gap-3.5">
+
+            {/* LOGO */}
             <img
-              src={favicon}
+              src={getFavicon(website.hostname)}
               alt={`Favicon of ${website.hostname}`}
               className="aspect-square h-10"
             />
+
+            {/* ORIGIN AND LINK */}
             <div className="truncate leading-0">
               <h3 className="truncate text-lg font-medium">
                 {website.hostname}
               </h3>
-              <a
-                href={website.origin}
+              <Link
+                to={website.origin}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-sm text-[var(--text-secondary)] hover:underline"
               >
                 {website.origin}
-              </a>
+              </Link>
             </div>
+            
           </div>
 
           {/* DETAILS */}
           <div className="w-full flex flex-col items-center justify-center gap-2.5">
 
-            {/* USERNAME */}
+            {/* USERNAME FIELD */}
             <div className="w-full py-3 px-3.5 flex flex-col gap-0.5 bg-gray-200/70 rounded-xl">
-              <span className="text-sm text-[var(--text-secondary)]">
+              <label className="text-sm text-[var(--text-secondary)]">
                 Username
-              </span>
+              </label>
               <div className="w-full flex items-center justify-between gap-3.5">
-                <p
-                  style={{ scrollbarWidth: "none" }}
-                  className="w-full text-[var(--text-primary)] text-nowrap overflow-x-auto"
-                >
-                  {data.username}
-                </p>
+                <input
+                  type="text"
+                  readOnly
+                  value={passwordData.username}
+                  className="w-full text-[var(--text-primary)] outline-none"
+                />
                 <button
-                  onClick={() => handleCopy(data.username)}
+                  onClick={() => handleCopy(passwordData.username)}
                   className="text-xl text-[var(--icon-secondary)] cursor-pointer hover:text-[var(--icon-primary)]"
                 >
                   <FiCopy />
@@ -106,22 +113,21 @@ function Password() {
               </div>
             </div>
 
-            {/* PASSWORD */}
+            {/* PASSWORD FIELD */}
             <div className="w-full py-3 px-3.5 flex flex-col gap-0.5 bg-gray-200/70 rounded-xl">
-              <span className="text-sm text-[var(--text-secondary)]">
+              <label className="text-sm text-[var(--text-secondary)]">
                 Password
-              </span>
+              </label>
               <div className="w-full flex items-center justify-between gap-3.5">
-                <p
-                  style={
-                    toggle
-                      ? { filter: "blur(0px)", scrollbarWidth: "none" }
-                      : { filter: "blur(5px)", scrollbarWidth: "none" }
-                  }
-                  className="w-full text-[var(--text-primary)] text-nowrap overflow-x-auto"
-                >
-                  {data.password}
-                </p>
+                <input
+                  type="text"
+                  readOnly
+                  value={passwordData.password}
+                  className={mergeClassNames(
+                    "w-full text-[var(--text-primary)] outline-none",
+                    toggle ? "blur-none" : "blur-xs"
+                  )}
+                />
                 <div className="text-[var(--icon-secondary)] flex items-center gap-2.5">
                   <button
                     onClick={() => setToggle((val) => !val)}
@@ -130,7 +136,7 @@ function Password() {
                     {toggle ? <VscEyeClosed /> : <VscEye />}
                   </button>
                   <button
-                    onClick={() => handleCopy(data.password)}
+                    onClick={() => handleCopy(passwordData.password)}
                     className="text-xl cursor-pointer hover:text-[var(--icon-primary)]"
                   >
                     <FiCopy />
@@ -141,22 +147,22 @@ function Password() {
 
           </div>
 
-          {/* BUTTON (LINKS) */}
+          {/* LINKS */}
           <div className="w-full text-center font-medium text-white flex items-center justify-center gap-3.5">
             <Link
-              to={`/vault/edit-password/${data._id}`}
+              to={`/vault/edit-password/${passwordData._id}`}
               className="w-full h-12 flex items-center justify-center cursor-pointer bg-[var(--theme-primary)] rounded-xl hover:bg-[var(--theme-secondary)]"
             >
               Edit
             </Link>
             <Link
-              to={`/vault/delete-password/${data._id}`}
+              to={`/vault/delete-password/${passwordData._id}`}
               className="w-full h-12 flex items-center justify-center cursor-pointer bg-red-500 rounded-xl hover:bg-red-400"
             >
               Delete
             </Link>
           </div>
-          
+
         </div>
       </main>
     );
