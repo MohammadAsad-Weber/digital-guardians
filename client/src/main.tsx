3867d95cb1f73878dc1287bed73bd55c3bbef9e8
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-08-20 15:50:45 +0530

    [Refactored]: Reorganized import statements for clarity and improved structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,13 @@
-import App from "@/App.tsx";
-import "@/assets/global.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 10, // 10 minutes
-    },
-  },
-});
+// App components & styles
+import App from "./app";
+import "./assets/global.css";
+
+// Data fetching & state management
+import { queryClient } from "./libs";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
